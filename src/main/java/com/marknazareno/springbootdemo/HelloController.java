6ab5959bb09537cf1e1ef49fae38c5f825c81075
Mark Nazareno <>
2021-06-05 00:40:10 +0800

    create demo app

---
+++ src/main/java/com/marknazareno/springbootdemo/HelloController.java
@@ -0,0 +1,16 @@
+package com.marknazareno.springbootdemo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping
+    public ResponseEntity<Message> getMessage() {
+        Message message = new Message("Hello World!");
+        return ResponseEntity.ok(message);
+    }
+}
---
+++ src/main/java/com/marknazareno/springbootdemo/Message.java
@@ -0,0 +1,10 @@
+package com.marknazareno.springbootdemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Message {
+    private String message;
+}
